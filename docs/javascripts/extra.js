cbf91bb40bbeea74552cf071cfbad1cd0d515024
xyu6 <yu.bill@hotmail.com>
2019-08-01 14:22:01 +0800

    change the extra.js

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -4,15 +4,25 @@
  */
 function checkLanguage() {
   var reg = /\/cn\//,
+  var regJp = /\/jp\//,
     oCNs = document.querySelectorAll('.cn'),
     oENs = document.querySelectorAll('.en'),
+    oJPs = document.querySelectorAll('.jp'),
     cnApi = 'http://wiki.seeedstudio.com/cn' + location.pathname;
+    jpApi = 'http://wiki.seeedstudio.com/jp' + location.pathname;
     enApi = 'http://wiki.seeedstudio.com' + location.pathname.slice(3);
   if (reg.test(location.href)) {
     isShow(oCNs, 'none')
+    isShow(oJPs, 'block',jpApi)
     isShow(oENs, 'block', enApi)
-  } else {
+  }else if(regJp.test(location.href)){
+    isShow(oJPs, 'none')
     isShow(oCNs, 'block', cnApi)
+    isShow(oENs, 'block', enApi)
+  }
+  else {
+    isShow(oCNs, 'block', cnApi)
+    isShow(oJPs, 'block', jpApi)
     isShow(oENs, 'none')
   }
 }
