65dc7baccb851fc6dbd5b6f4b2e7befa82777827
ainsfei <solarrainyjm@gmail.com>
2019-08-02 11:37:39 +0800

    add

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -4,37 +4,36 @@
  */
 function checkLanguage() {
   var reg = /\/cn\//,
-  var regJp = /\/jp\//,
-    oCNs = document.querySelectorAll('.cn'),
-    oENs = document.querySelectorAll('.en'),
-    oJPs = document.querySelectorAll('.jp'),
-    cnApi = 'http://wiki.seeedstudio.com/cn' + location.pathname;
-    jpApi = 'http://wiki.seeedstudio.com/jp' + location.pathname;
-    enApi = 'http://wiki.seeedstudio.com' + location.pathname.slice(3);
+      regJp = /\/jp\//,
+      oCNs = document.querySelectorAll('.cn'),
+      oENs = document.querySelectorAll('.en'),
+      oJPs = document.querySelectorAll('.jp'),
+      cnApi = 'http://wiki.seeedstudio.com/cn' + location.pathname.replace(/^\/jp/,""),
+      jpApi = 'http://wiki.seeedstudio.com/jp' + location.pathname.replace(/^\/cn/,""),
+      enApi = 'http://wiki.seeedstudio.com' + location.pathname.replace(/^(\/cn|\/jp)/,"");
   if (reg.test(location.href)) {
-    isShow(oCNs, 'none')
-    isShow(oJPs, 'block',jpApi)
-    isShow(oENs, 'block', enApi)
+    isShow(oCNs, 'none');
+    isShow(oJPs, 'block',jpApi);
+    isShow(oENs, 'block', enApi);
   }else if(regJp.test(location.href)){
-    isShow(oJPs, 'none')
-    isShow(oCNs, 'block', cnApi)
-    isShow(oENs, 'block', enApi)
-  }
-  else {
-    isShow(oCNs, 'block', cnApi)
-    isShow(oJPs, 'block', jpApi)
-    isShow(oENs, 'none')
+    isShow(oJPs, 'none');
+    isShow(oCNs, 'block', cnApi);
+    isShow(oENs, 'block', enApi);
+  } else {
+    isShow(oCNs, 'block', cnApi);
+    isShow(oJPs, 'block', jpApi);
+    isShow(oENs, 'none');
   }
 }
 
 function isShow(obj, style, url) {
   if (!url) {
     obj.forEach(function(item, key) {
-      item.style.display = style
+      item.style.display = style;
     })
   } else {
     obj.forEach(function(item, key) {
-      item.style.display = style
+      item.style.display = style;
       item.onclick = function(){
           location.href = url;
       }
@@ -45,6 +44,9 @@ function isShow(obj, style, url) {
 checkLanguage();
 
 // 邮箱输入 阻止F/S触发搜索聚焦
-document.getElementById("mce-EMAIL").addEventListener("keydown",function(e){
+if(document.getElementById("mce-EMAIL")){
+  document.getElementById("mce-EMAIL").addEventListener("keydown",function(e){
   window.event ? window.event.cancelBubble = true : e.stopPropagation();
-},false);  
+},false); 
+}
+ 
