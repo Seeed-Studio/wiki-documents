fe8c187f6c1116d86fdb985deb42787eacd06082
w0x7ce <tian.r.king@gmail.com>
2022-12-01 02:03:11 -0500

    Fix: /docs -> /

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -187,7 +187,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/About',
+                to: '/About',
               },
             ],
           },
