915faed9db6bb952709568a8cbdd0362c4001a34
w0x7ce <tian.r.king@gmail.com>
2023-01-03 13:15:50 +0800

    Update: ODYSSEY-X86J4105-LTE-Module.md

--- sidebars.js
+++ sidebars.js
@@ -165,7 +165,8 @@ const sidebars = {
             'ODYSSEY/ODYSSEY_X86J4105/ODYSSEY-X86J4105-Installing-FreeNAS',
             'ODYSSEY/ODYSSEY_X86J4105/ODYSSEY-X86J4105-Intel-OpenVINO',
             'ODYSSEY/ODYSSEY_X86J4105/ODYSSEY-X86J4105-NCS2',
-            'ODYSSEY/ODYSSEY_X86J4105/Coral-Mini-PCIe-Accelerator-on-ODYSSEY-X86J4105'
+            'ODYSSEY/ODYSSEY_X86J4105/Coral-Mini-PCIe-Accelerator-on-ODYSSEY-X86J4105',
+            'ODYSSEY/ODYSSEY_X86J4105/ODYSSEY-X86J4105-LTE-Module',
           ]
         },
 
