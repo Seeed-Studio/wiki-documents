350a9fc191fcaed3cce3f9d3e8c6412cf6e4de13
w0x7ce <tian.r.king@gmail.com>
2023-01-03 13:01:27 +0800

    Update: ODYSSEY-X86J4105-Intel-OpenVINO.md

--- sidebars.js
+++ sidebars.js
@@ -163,6 +163,7 @@ const sidebars = {
             'ODYSSEY/ODYSSEY_X86J4105/ODYSSEY-X86J4105-Installing-Android',
             'ODYSSEY/ODYSSEY_X86J4105/ODYSSEY-X86J4105-AzureIOT',
             'ODYSSEY/ODYSSEY_X86J4105/ODYSSEY-X86J4105-Installing-FreeNAS',
+            'ODYSSEY/ODYSSEY_X86J4105/ODYSSEY-X86J4105-Intel-OpenVINO',
           ]
         },
 
