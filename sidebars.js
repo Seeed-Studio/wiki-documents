634ab250825c8e7e342b3d1f1c6aef325a62be1c
w0x7ce <tian.r.king@gmail.com>
2023-01-10 15:05:49 +0800

    Update: Seeeduino-Crypto-ATmega4809-ECC608.md

--- sidebars.js
+++ sidebars.js
@@ -41,8 +41,7 @@ const sidebars = {
     },
 
     {
-
-
+      
       type: 'category',
       label: 'Arduino',
       items: [
@@ -63,6 +62,7 @@ const sidebars = {
         // 'Arduino/Seeeduino_v4.0',
         'Arduino/Seeeduino_v4.2',
         'Arduino/SeeedStudio-GD32-RISC-V-Dev-Board',
+        'Arduino/Seeeduino-Crypto-ATmega4809-ECC608',
       ],
 
     },
