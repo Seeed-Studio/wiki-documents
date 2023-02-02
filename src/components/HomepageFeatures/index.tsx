beb7721656d2f3889759884940a5b3353a508885
BilltheWorld <2768073931@qq.com>
2023-02-02 14:40:07 +0800

    Update: index page update

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'All Files Stored',
+    Svg: require('@site/static/img/documentation.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+      All the Seeed Studio Production Files <br/> Sensor modules, Development boards, Devices, Cloud, Solutions: <br/>Everything you need for <strong>the True Wild</strong>.  
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Contributors Inspired',
+    Svg: require('@site/static/img/people.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+      Anything Editable for Anyone Anytime <br/> Seeed Studio Controbutors Program Supported
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Brand New Framework',
+    Svg: require('@site/static/img/structure.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+      New structure, New categorization <br/>More function, Better exhibition <br/><code>Algolia</code>  Serach Engine Powered<br/> Built around <code>Docusaurus</code> Framework
       </>
     ),
   },
