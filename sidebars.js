da6b2dfa1616d69d1154ad55261c7f946e68c6e3
159joe <330910206@qq.com>
2023-02-14 18:00:17 +0800

    Audit a part of grove

--- sidebars.js
+++ sidebars.js
@@ -476,7 +476,7 @@ const sidebars = {
                 'Sensor/Grove/Grove_Accessories/Actuator/Grove-MP3-v3',
                 // 'Sensor/Grove/Grove_Accessories/Actuator/Grove-Mini_Fan',
                 // 'Sensor/Grove/Grove_Accessories/Actuator/Grove-Mini_I2C_Motor_Driver_v1.0',
-                // 'Sensor/Grove/Grove_Accessories/Actuator/Grove_Recorder',
+                'Sensor/Grove/Grove_Accessories/Actuator/Grove_Recorder',
                 'Sensor/Grove/Grove_Accessories/Actuator/Grove-Recorder_v3.0',
                 // 'Sensor/Grove/Grove_Accessories/Actuator/Grove-Relay', 
                 'Sensor/Grove/Grove_Accessories/Actuator/Grove-4-Channel_SPDT_Relay',
@@ -2051,6 +2051,22 @@ const sidebars = {
 
 
           ]
+        },
+        {
+          type: 'category',
+          label: 'Kit with Courses',
+          items: [
+            {
+              type: 'category',
+              label: 'Grove Beginner Kit for Arduino',
+              items: [
+                'Top_Brand/Arduino/Kit with Courses/Grove Beginner Kit for Arduino/Grove-Beginner-Kit-For-Arduino',
+                'Top_Brand/Arduino/Kit with Courses/Grove Beginner Kit for Arduino/Grove-Beginner-Kit-for-Arduino-education-pack',
+                'Top_Brand/Arduino/Kit with Courses/Grove Beginner Kit for Arduino/Grove-Beginner-Kit-for-Arduino-Upverter-Guide',
+                'Top_Brand/Arduino/Kit with Courses/Grove Beginner Kit for Arduino/Grove_Beginner_Kit_for_Arduino',
+              ],
+            },
+          ],
         },
       ],
 
