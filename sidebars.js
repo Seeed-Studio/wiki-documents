3030ec78462b1c03cccc306a84b87d1ac68c215f
Lakshantha Dissanayake <lakshanthad@yahoo.com>
2023-03-27 10:58:54 +0800

    Add: Cochl.Sense on NVIDIA Jetson wiki

--- sidebars.js
+++ sidebars.js
@@ -2048,6 +2048,7 @@ const sidebars = {
             'Edge/reComputer/Software/Update-Jetson-Linux-OTA-Using-Allxon',
             'Edge/reComputer/Software/alwaysAI-Jetson-Getting-Started',
             'Edge/reComputer/Software/reComputer_Jetson_Series_Resource',
+            'Edge/reComputer/Software/Cochl.Sense-Jetson-Getting-Started',
           ]
         },
         {
