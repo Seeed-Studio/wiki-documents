a4e7bf2688751a74d138764628f061376b28b01b
MatthewJeffson <2768073931@qq.com>
2023-05-25 09:39:29 +0800

    Add: add trailingSlash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Seeed-Studio', // Usually your GitHub org/user name.
   projectName: 'wiki-documents', // Usually your repo name.
-
+  trailingSlash: true,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
