4172cefe9d9089af0b8e66e2c409034ecfaa734f
lisha <sha.li@seeed.cc>
2023-07-18 15:39:00 +0800

    feat: recomputer page background

--- src/components/recomputer/index.tsx
+++ src/components/recomputer/index.tsx
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import './index.css';
 
 const BrandSceneList = () => {
-  const [selectedBrand, setSelectedBrand] = useState(null);
+  const [selectedBrand, setSelectedBrand] = useState([]);
   const [selectedScenes, setSelectedScenes] = useState([]);
   const brands = [
-    { id: 1, name: 'CVEDIA-RT',link:'https://www.baidu.com/', scenes: [4] },
-    { id: 2, name: 'Lumeo', link:'https://www.baidu.com/',scenes: [4] },
-    { id: 3, name: 'alwaysAI', link:'https://www.baidu.com/',scenes: [1] },
-    { id: 4, name: 'YOLOv8',link:'https://www.baidu.com/', scenes: [1, 2, 4] },
-    { id: 5, name: 'YOLOv5',link:'https://www.baidu.com/', scenes: [2, 4] },
-    { id: 6, name: 'roboflow',link:'https://www.baidu.com/', scenes: [1, 4] },
-    { id: 7, name: 'Allxon1',link:'https://www.baidu.com/', scenes: [5] },
-    { id: 8, name: 'Deci',link:'https://www.baidu.com/', scenes: [3] },
-    { id: 9, name: 'Edge Impulse',link:'https://www.baidu.com/', scenes: [1, 2, 4] },
-    { id: 10, name: 'Cochl.Sense',link:'https://www.baidu.com/', scenes: [4] },
+    { id: 1, name: 'CVEDIA-RT',link:'https://www.baidu.com/',bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/cvedia-logo.jpg', scenes: [4] },
+    { id: 2, name: 'Lumeo', link:'https://www.baidu.com/',bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/lumeo-logo.jpg', scenes: [4] },
+    { id: 3, name: 'alwaysAI', link:'https://www.baidu.com/',bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/jetsonaitools83.png', scenes: [1] },
+    { id: 4, name: 'YOLOv8',link:'https://www.baidu.com/', bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/yolov8-logo.png', scenes: [1, 2, 4] },
+    { id: 5, name: 'YOLOv5',link:'https://www.baidu.com/', bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/jetsonaitools80.png', scenes: [2, 4] },
+    { id: 6, name: 'roboflow',link:'https://www.baidu.com/', bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/jetsonaitools81.png', scenes: [1, 4] },
+    { id: 7, name: 'Allxon1',link:'https://www.baidu.com/', bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/jetsonaitools4.png', scenes: [5] },
+    { id: 8, name: 'Deci',link:'https://www.baidu.com/', bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/jetsonaitools82.png', scenes: [3] },
+    { id: 9, name: 'Edge Impulse',link:'https://www.baidu.com/', bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/ei-logo.png', scenes: [1, 2, 4] },
+    { id: 10, name: 'Cochl.Sense',link:'https://www.baidu.com/', bg:'https://files.seeedstudio.com/wiki/recomputerzhongwen/cochl-logo.png', scenes: [4] },
   ];
   const scenes = [
     { id: 1, name: 'Data Upload & Label',link:'https://www.baidu.com/',img:'huang.png', brands: [3, 4, 6, 9]},
@@ -24,7 +24,7 @@ const BrandSceneList = () => {
     { id: 5, name: 'Remote Manage',link:'https://www.baidu.com/', img:'huang.png',brands: [7] },
   ];
   const handleBrandMouseEnter = (brand) => {
-    setSelectedBrand(brand);
+    setSelectedBrand([brand]);
     const associatedScenes = scenes.filter((scene) =>
       scene.brands.includes(brand.id)
     );
@@ -32,17 +32,16 @@ const BrandSceneList = () => {
   };
 
   const handleSceneMouseEnter = (scene) => {
-    console.log(scene);
     
-    setSelectedBrand(null);
+    setSelectedBrand([]);
     const associatedBrandIds = scene.brands;
     const associatedBrands = brands.filter((brand) =>
       associatedBrandIds.includes(brand.id)
-    );
-    console.log(associatedBrands[0].id);
+    )
+    console.log(associatedBrands);
     
     setSelectedScenes([scene]);
-    associatedBrands.length&&setSelectedBrand(associatedBrands[0].id);
+    associatedBrands.length&&setSelectedBrand(associatedBrands);
   };
 
   const getBrandNameById = (brandId) => {
@@ -51,7 +50,7 @@ const BrandSceneList = () => {
   };
 
 const handleMouseLeave=()=>{
-  setSelectedBrand(null);
+  setSelectedBrand([]);
   setSelectedScenes([]);
 }
   return (
@@ -64,8 +63,9 @@ const handleMouseLeave=()=>{
               onMouseEnter={() => handleBrandMouseEnter(brand)}
               onMouseLeave={handleMouseLeave}
                             className={
-              `padding--sm ${selectedBrand === brand.id  ? 'selected' : ''}`
+              `padding--sm ${selectedBrand.find((d) => d.id === brand.id)   ? 'selected' : ''}`
             }
+            style={{backgroundImage:selectedBrand.find((d) => d.id === brand.id)   ? `url(${brand.bg})` :''}}
             >
               <a target='_blank' href={brand.link}>{brand.name}</a>
             </li>
