bb3ed02ebe90053972ffa0ce19a1087f009f870c
Corey Thompson <corey.dwayne.thompson@gmail.com>
2023-08-19 07:50:24 -0500

    use components directly in mdx. features work

--- src/components/raspberrypi/value.tsx
+++ src/components/raspberrypi/value.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './value.css';
 
+const featureContents =[
+  {"rpifeature1":"The Raspberry Pi is a low-cost computer that is affordable for everyone. The price of a Raspberry Pi ranges from $5 to $55, depending on the model"},
+  {"rpifeature2":"The Raspberry Pi can be used for a wide range of projects, from home automation to gaming to robotics. It can also run a variety of operating systems, including Linux and Android"},
+  {"rpifeature3":"The Raspberry Pi is easy to set up and use, even for beginners. It comes with a stable Raspbian operating system and a huge number of enthusiasts are constantly developing new applications for it"},
+  {"rpifeature4":"The Raspberry Pi is small and lightweight, making it easy to carry around and use in different locations"}
+]
 const value = () => {
+
   return (
     <div>
-    <h2>Why Do People Love Raspberry Pi?</h2>
       <div className='feature-container'>
-        {/* <div className='feature'>Affordability: The Raspberry Pi is a low-cost computer that is affordable for everyone. The price of a Raspberry Pi ranges from $5 to $55, depending on the model</div>
-        <div className='feature'>Versatility: The Raspberry Pi can be used for a wide range of projects, from home automation to gaming to robotics. It can also run a variety of operating systems, including Linux and Android</div>
-        <div className='feature'>Ease of use: The Raspberry Pi is easy to set up and use, even for beginners. It comes with a stable Raspbian operating system and a huge number of enthusiasts are constantly developing new applications for it</div>
-        <div className='feature'>Size: The Raspberry Pi is small and lightweight, making it easy to carry around and use in different locations</div> */}
-            <div className='feature'>Affordability</div>
-            <div className='feature'>Versatility</div>
-            <div className='feature'>Ease of use</div>
-            <div className='feature'>Size</div>
-        </div>
+            <div id='rpifeature1' className='feature'>💰 Affordability</div>
+            <div id='rpifeature2' className='feature'>🧩 Versatility</div>
+            <div id='rpifeature3' className='feature'>🚀 Ease of use</div>
+            <div id='rpifeature4' className='feature'>🐜 Size</div>
+      </div>
+      <div className='feature-explanation'>
+        {/* todo: make variable on hover */}
+        <p id='rpifeaturecontent'>The Raspberry Pi is a low-cost computer that is affordable for everyone. The price of a Raspberry Pi ranges from $5 to $55, depending on the model</p>
+      </div>
     </div>
   );
 };
