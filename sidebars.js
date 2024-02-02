b44895454442f7380515cbc4459ef62804495d0b
QingWind6 <993628705@qq.com>
2024-02-02 16:34:14 +0800

    Add new wiki about R4 and E5

--- sidebars.js
+++ sidebars.js
@@ -428,6 +428,8 @@ const sidebars = {
                     'Sensor/Grove/Grove_Sensors_Network/LoRa/demo/Grove_Wio_E5_TTN_Demo',
                     'Sensor/Grove/Grove_Sensors_Network/LoRa/demo/Grove_Wio_E5_Helium_Demo',
                     'Sensor/Grove/Grove_Sensors_Network/LoRa/demo/Grove_Wio_E5_Helium_tinyML_Demo',
+                    'Sensor/Grove/Grove_Sensors_Network/LoRa/demo/How to Use Arduino Uno R4 with LoRa Module'
+
               ],
             },
             {
