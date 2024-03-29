bcbc1726eca35ca77cda5de48d5509647824a9aa
Matthew <104187183+MatthewJeffson@users.noreply.github.com>
2024-03-29 16:44:58 +0800

    Add: add neqto engine for linux file

--- sidebars.js
+++ sidebars.js
@@ -2659,6 +2659,7 @@ const sidebars = {
               type: 'category',
               label: 'Software with AI methods',
               items: [
+                'Edge/NVIDIA_Jetson/reComputer/Software/neqto_engine_for_linux_recomputer',
                 'Edge/NVIDIA_Jetson/reComputer/Software/CVEDIA-Jetson-Getting-Started',
                 'Edge/NVIDIA_Jetson/reComputer/Software/Lumeo-Jetson-Getting-Started',
                 'Edge/NVIDIA_Jetson/reComputer/Software/DeciAI-Getting-Started',
@@ -2713,7 +2714,7 @@ const sidebars = {
                 'Edge/NVIDIA_Jetson/reComputer/Application/How_to_Format_the_Output_of_LLM_Using_Langchain_on_Jetson',
                 'Edge/NVIDIA_Jetson/reComputer/Application/Deploy_Riva_and_Llama2_on_reComputer',
                 'Edge/NVIDIA_Jetson/reComputer/Application/Real_Time_Subtitle_Recoder_on_Jetson',
-                'Edge/NVIDIA_Jetson/reComputer/Application/Whisper_on_Jetson_for_Real_Time_Speech_to_Text'
+                'Edge/NVIDIA_Jetson/reComputer/Application/Whisper_on_Jetson_for_Real_Time_Speech_to_Text',
               ],
             },
 
