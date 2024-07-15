d0e549f4ea5eab77d5464287248b1710b1d95a9b
ShuishengPeng <63339353+ackPeng@users.noreply.github.com>
2024-07-15 17:37:28 +0800

    add modbus rtu for recomputer_r1000_with_fin_use_modbus

--- sidebars.js
+++ sidebars.js
@@ -2972,7 +2972,7 @@ const sidebars = {
                             label: 'FIN',
                             items: [
                                'Edge/Raspberry_Pi_Devices/reComputer_R1000/Applications/Fin/recomputer_r1000_install_fin',
-                               'Edge/Raspberry_Pi_Devices/reComputer_R1000/Applications/Fin/recomputer_r1000_with_fin_use_modbus_tcp',
+                               'Edge/Raspberry_Pi_Devices/reComputer_R1000/Applications/Fin/recomputer_r1000_with_fin_use_modbus',
                               
                               ],
                             },
