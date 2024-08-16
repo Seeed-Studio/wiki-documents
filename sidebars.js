d8ac6ee7f17c6a35a550d18c764fbadec088ca0f
Jessie219-web <112080869+Jessie219-web@users.noreply.github.com>
2024-08-16 14:22:09 +0800

    add respeaker button usage

--- sidebars.js
+++ sidebars.js
@@ -1696,6 +1696,7 @@ const sidebars = {
           },
           items: [
             'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_i2s_test',
+            'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_button',
             'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_record_and_play',
            // 'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_i2s_rgb',
             {
