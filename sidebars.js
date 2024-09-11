e36437804e6b1b4c44c67e777c2a3907792f4523
Citric Lee <37475446+limengdu@users.noreply.github.com>
2024-09-11 17:41:48 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1288,13 +1288,8 @@ const sidebars = {
               items: [
                 'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32S3/XIAO-ESP32S3-Zephyr-RTOS',
                 'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32S3/XIAO_ESP32S3_with_MicroPython',
-<<<<<<< docusaurus-version
-                'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32S3/XIAO_ESP32S3_Project_Micropython',
-                'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32S3/XIAO_ESP32S3_Project_CircuitPython',
-=======
                 'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32S3/XIAO_ESP32S3_Micropython',
                 'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32S3/XIAO-ESP32S3-FreeRTOS',
->>>>>>> docusaurus-version
               ],
             },
             {
