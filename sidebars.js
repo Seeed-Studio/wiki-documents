663ced953f356193e0cab94486d22e6f325e716e
Jessie219-web <112080869+Jessie219-web@users.noreply.github.com>
2024-09-29 09:12:37 +0800

    add respeaker examples

--- sidebars.js
+++ sidebars.js
@@ -1766,6 +1766,8 @@ const sidebars = {
           },
           items: [
             'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_i2s_test',
+            'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_rgb_test',
+            'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_volume',
             'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_button',
             'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_record_and_play',
            // 'Sensor/ReSpeaker_Lite/XIAO_ESP32_S3/respeaker_i2s_rgb',
