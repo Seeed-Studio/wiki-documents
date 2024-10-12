978d5140eaa7c6536d60cac7ca9bd80000af707d
lidayu <2334721338@qq.com>
2024-10-12 12:59:06 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -3450,6 +3450,7 @@ const sidebars = {
                         // },
                   items: [
                     'Edge/NVIDIA_Jetson/Application/Robotics/Hardware/Robosense_Lidar',
+		    'Edge/NVIDIA_Jetson/Application/Robotics/Hardware/Orbbec_Depth_Camera',
                     'Edge/NVIDIA_Jetson/Application/Robotics/Hardware/CSI_Camera'
                       ]},
                 
