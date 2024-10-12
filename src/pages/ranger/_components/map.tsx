d3b80bbe89418ac7fda276eb5b2b3d2ce61bce09
Matthew <104187183+MatthewJeffson@users.noreply.github.com>
2024-10-12 17:57:44 +0800

    Update map.tsx

--- src/pages/ranger/_components/map.tsx
+++ src/pages/ranger/_components/map.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect } from 'react';
-function ThirdPartyComponent() {
+
+const ThirdPartyComponent: React.FC = () => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs';
-    script.type = 'module';
-    document.body.appendChild(script);
+    // Check if the script is already loaded
+    if (!document.querySelector('[src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"]')) {
+      const script = document.createElement('script');
+      script.src = 'https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs';
+      script.type = 'module';
+      document.body.appendChild(script);
+    }
   }, []);
 
   return (
     <div>
-      <dotlottie-player src="https://lottie.host/1d94a44a-9bcc-47b5-b738-5792c00d4563/zLPxvIpMVk.json" background="transparent" speed="1" 
-        style={{ width: "100%",maxWidth:'1288px', height: "auto" ,margin:'0 auto'}} loop autoplay></dotlottie-player>
+      <dotlottie-player src="https://lottie.host/107d7909-963b-4494-9761-a2d7437d594c/49g2fCLnXH.json" background="transparent" speed="1" 
+        style={{  width: "100%",maxWidth:'1288px', height: "auto" ,margin:'0 auto' }} loop autoplay></dotlottie-player>
     </div>
   );
-}
+};
 
 export default ThirdPartyComponent;
