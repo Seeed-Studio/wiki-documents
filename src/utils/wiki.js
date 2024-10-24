ea87282c3e87cc2d8d859a2129c70fd12d07f7e3
nfs0619 <1447996181@qq.com>
2024-10-24 15:33:40 +0800

    update wiki.js

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -1,86 +1,86 @@
 const docList = [
   [
     {
-      "path": "/wio_sx1262",
-      "image": "https://files.seeedstudio.com/wiki/XIAO_ESP32S3_for_Meshtastic_LoRa/34.png",
-      "title": "Wio-SX1262 Introduction",
-      "date": "10/12/2024"
+      "path": "/weekly_wiki",
+      "image": "https://files.seeedstudio.com/wiki/IndexWiki/logo_image.jpg",
+      "title": "Weekly Wiki",
+      "date": "10/21/2024"
     },
     {
-      "path": "/orbbec_depth_camera_on_ros",
-      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
-      "title": "Orbbec Depth Camera with ROS",
-      "date": "10/10/2024"
+      "path": "/getting_started_with_watcher_task",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/85.png",
+      "title": "Task Assignment Guideline",
+      "date": "10/18/2024"
     }
   ],
   [
     {
-      "path": "/recomputer_r1000_use_modbus_rtu_with_codesys",
-      "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01.png",
-      "title": "How to use CODESYS to configure the Modbus rtu function of R1000",
-      "date": "10/10/2024"
+      "path": "/watcher_hardware_overview",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/hardware_overview.jpg",
+      "title": "Hardware overview",
+      "date": "10/18/2024"
     },
     {
-      "path": "/SenseCAP_Indicator_Application_Home_Assistant",
-      "image": "https://files.seeedstudio.com/wiki/SenseCAP/SenseCAP_Indicator/indicator-ha-thumbnail.jpg",
-      "title": "Home Assistant - SenseCAP Indicator",
-      "date": "10/09/2024"
+      "path": "/watcher_operation_guideline",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/0.JPG",
+      "title": "Operation Guideline",
+      "date": "10/18/2024"
     }
   ],
   [
     {
-      "path": "/watcher_software_service_framework",
-      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/50.jpg",
-      "title": "7# Watcher Software Service Framework",
-      "date": "10/09/2024"
+      "path": "/watcher_price",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/price_month_simpler.png",
+      "title": "Price and Benefits",
+      "date": "10/18/2024"
     },
     {
-      "path": "/sensecraft_ai_for_watcher",
-      "image": "https://files.seeedstudio.com/wiki/SenseCraft_Watcher/website/top3.png",
-      "title": "SenseCraft Watcher",
-      "date": "10/9/2024"
+      "path": "/sensecap_app_introduction",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/30.png",
+      "title": "SenseCraft APP Introduction",
+      "date": "10/18/2024"
     }
   ],
   [
     {
-      "path": "/wio_sx1262_xiao_esp32s3_for_lora_get_started",
+      "path": "/watcher_local_deploy",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/87.png",
+      "title": "Deploy Watcher's AI capabilities locally",
+      "date": "10/18/2024"
+    },
+    {
+      "path": "/getting_started_with_watcher",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/watcherKS.jpg",
+      "title": "Quick Start Guide",
+      "date": "10/17/2024"
+    }
+  ],
+  [
+    {
+      "path": "/wio_sx1262_xiao_esp32s3_LNS_Chirpstack",
       "image": "https://files.seeedstudio.com/wiki/XIAO_ESP32S3_for_Meshtastic_LoRa/2.png",
-      "title": "Get Started for Lora",
-      "date": "10/09/2024"
+      "title": "Connect to Chirpstack",
+      "date": "10/17/2024"
     },
     {
-      "path": "/build_balenaOS_for_reComputer_r1000",
-      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
-      "title": "Build balenaOS for reComputer R1000",
-      "date": "10/8/2024"
+      "path": "/wio_sx1262_xiao_esp32s3_LNS_TTN",
+      "image": "https://files.seeedstudio.com/wiki/XIAO_ESP32S3_for_Meshtastic_LoRa/40.png",
+      "title": "Connect to TTN",
+      "date": "10/17/2024"
     }
   ],
   [
     {
-      "path": "/recomputer_r1000_install_codesys",
-      "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01.png",
-      "title": "How to install CODESYS for reComputer R1000",
-      "date": "10/8/2024"
+      "path": "/XIAO_BLE",
+      "image": "https://files.seeedstudio.com/wiki/XIAO-BLE/102010469_Front-14.jpg",
+      "title": "Getting Started with XIAO nRF52840",
+      "date": "2024-10-15T03:15:24.000Z"
     },
     {
-      "path": "/reComputer_r1000_fuxa_achieve_scada",
-      "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01.png",
-      "title": "reComputer R1000 with fuxa to achieve SCADA",
-      "date": "10/8/2024"
-    }
-  ],
-  [
-    {
-      "path": "/reComputer_r1000_fuxa_modbus_rtu_and_tcp",
-      "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01.png",
-      "title": "reComputer R1000 with fuxa to use modbus RTU/TCP",
-      "date": "10/8/2024"
-    },
-    {
-      "path": "/reComputer_r1000_fuxa_mqtt_client",
-      "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01.png",
-      "title": "reComputer R1000 with fuxa to use mqtt client",
-      "date": "10/8/2024"
+      "path": "/ha_with_mr60fda2",
+      "image": "https://files.seeedstudio.com/wiki/mmwave-for-xiao/mr60/2-mmWave-45font.jpg",
+      "title": "MR60FDA2 Fall detection Sensor with Home Assistant",
+      "date": "10/15/2024"
     }
   ]
 ];
