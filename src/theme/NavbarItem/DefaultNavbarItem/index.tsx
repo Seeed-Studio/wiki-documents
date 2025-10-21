6fc47b4e5a842f503b027ca9b97d31ab536f58ea
nfs0619 <1447996181@qq.com>
2025-10-21 16:35:37 +0800

    Fix incorrect links in header and footer for multilingual documents

--- src/theme/NavbarItem/DefaultNavbarItem/index.tsx
+++ src/theme/NavbarItem/DefaultNavbarItem/index.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 import OriginalDefaultNavbarItem from '@theme-original/NavbarItem/DefaultNavbarItem';
 import {useLocation} from '@docusaurus/router';
 
-import {
-  detectLocaleFromPath,
-  localizeHref,
-  isLocaleHomePath,
-} from '../../../pages/home/lib/locale';
+import {detectLocaleFromPath, localizeHref} from '../../../pages/home/lib/locale';
 import {translateNavbarLabel} from '../navLabelHelper';
 
 export default function DefaultNavbarItemWrapper(
-  props: React.ComponentProps<typeof OriginalDefaultNavbarItem>
+  props: React.ComponentProps<typeof OriginalDefaultNavbarItem>,
 ) {
   const {pathname} = useLocation();
   const locale = detectLocaleFromPath(pathname);
-  const isHome = isLocaleHomePath(pathname);
 
   const nextProps = {...props} as any;
 
@@ -22,13 +17,11 @@ export default function DefaultNavbarItemWrapper(
     nextProps.label = translateNavbarLabel(nextProps.label, locale);
   }
 
-  // 仅站内/wiki 链接加前缀；外链保持不变
-  if (isHome) {
-    if (typeof nextProps.to === 'string') {
-      nextProps.to = localizeHref(nextProps.to, locale);
-    } else if (typeof nextProps.href === 'string') {
-      nextProps.href = localizeHref(nextProps.href, locale);
-    }
+  // 仅为站内 wiki 链接加上对应语言前缀，外部链接保持原状。
+  if (typeof nextProps.to === 'string') {
+    nextProps.to = localizeHref(nextProps.to, locale);
+  } else if (typeof nextProps.href === 'string') {
+    nextProps.href = localizeHref(nextProps.href, locale);
   }
 
   return <OriginalDefaultNavbarItem {...nextProps} />;
--- src/theme/NavbarItem/DropdownNavbarItem/index.tsx
+++ src/theme/NavbarItem/DropdownNavbarItem/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import OriginalDropdownNavbarItem from '@theme-original/NavbarItem/DropdownNavbarItem';
 import {useLocation} from '@docusaurus/router';
 
-import {
-  detectLocaleFromPath,
-  localizeHref,
-  isLocaleHomePath,
-} from '../../../pages/home/lib/locale';
+import {detectLocaleFromPath, localizeHref} from '../../../pages/home/lib/locale';
 import {translateNavbarLabel} from '../navLabelHelper';
 
 export default function DropdownNavbarItemWrapper(
@@ -14,7 +10,6 @@ export default function DropdownNavbarItemWrapper(
 ) {
   const {pathname} = useLocation();
   const locale = detectLocaleFromPath(pathname);
-  const isHome = isLocaleHomePath(pathname);
 
   const nextProps = {...props} as any;
 
@@ -28,23 +23,19 @@ export default function DropdownNavbarItemWrapper(
       if (nextItem.label) {
         nextItem.label = translateNavbarLabel(nextItem.label, locale);
       }
-      if (isHome) {
-        if (typeof nextItem.to === 'string') {
-          nextItem.to = localizeHref(nextItem.to, locale);
-        } else if (typeof nextItem.href === 'string') {
-          nextItem.href = localizeHref(nextItem.href, locale);
-        }
+      if (typeof nextItem.to === 'string') {
+        nextItem.to = localizeHref(nextItem.to, locale);
+      } else if (typeof nextItem.href === 'string') {
+        nextItem.href = localizeHref(nextItem.href, locale);
       }
       return nextItem;
     });
   }
 
-  if (isHome) {
-    if (typeof nextProps.to === 'string') {
-      nextProps.to = localizeHref(nextProps.to, locale);
-    } else if (typeof nextProps.href === 'string') {
-      nextProps.href = localizeHref(nextProps.href, locale);
-    }
+  if (typeof nextProps.to === 'string') {
+    nextProps.to = localizeHref(nextProps.to, locale);
+  } else if (typeof nextProps.href === 'string') {
+    nextProps.href = localizeHref(nextProps.href, locale);
   }
 
   return <OriginalDropdownNavbarItem {...nextProps} />;
