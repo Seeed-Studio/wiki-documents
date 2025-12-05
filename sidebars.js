33ae9e4addd8625fdc4459eb9019ca9d7885ef05
zibo <102125578+zibochen6@users.noreply.github.com>
2025-12-05 17:55:11 +0800

    Update recomputer OTA doc
    Update recomputer OTA doc

--- sidebars.js
+++ sidebars.js
@@ -3830,6 +3830,7 @@ const sidebars = {
           items: [
             'Edge/NVIDIA_Jetson/FAQs/jetson_FAQ',
             'Edge/NVIDIA_Jetson/FAQs/Create_Backup_and_Restore_on_reComputer',
+            'Edge/NVIDIA_Jetson/FAQs/Deploy_OTA_ON_reComputer',
             // 'Edge/NVIDIA_Jetson/FAQs/Updating_Jetpack_with_OTA',
             'Edge/NVIDIA_Jetson/FAQs/Solution_for_the_Compatibility_Issue_between_reComputer_and_VEYE_Camera',
             'Edge/NVIDIA_Jetson/FAQs/Use_IMX477_Camera_with_A603',
