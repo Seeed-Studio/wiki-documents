d42018e84fa9afa323f0a3318a1dd9d2546276d4
Kian-kang <kangwei000513@qq.com>
2026-03-18 14:51:43 +0800

    Add:<S2100>+<Analog input resolution>;Fix:<SenseCAP One Compact Weather Sensor>+<Changes to the USB Port Diagram>;Add:<R1225>+<Qucik Start>

--- sites/en/sidebars.js
+++ sites/en/sidebars.js
@@ -2687,6 +2687,7 @@ const sidebars = {
           label: 'reComputer R1225 LoRaWAN Gateway',
           items: [
             'Network/SenseCAP_Network/reComputer_R1225_LoRaWAN_Gateway/R1225_introduction',
+            'Network/SenseCAP_Network/reComputer_R1225_LoRaWAN_Gateway/R1225_Quick_Start',
           ],
         },
         {
