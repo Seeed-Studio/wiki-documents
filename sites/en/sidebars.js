ff1f3b79e8168b86904968b7e564a2acb1a7256c
zibochen6 <www.1691125058@foxmail.com>
2026-04-14 15:12:33 +0800

    update rtl8852be wiki

--- sites/en/sidebars.js
+++ sites/en/sidebars.js
@@ -3912,6 +3912,7 @@ const sidebars = {
               items: [
                 'Edge/NVIDIA_Jetson/Other_Devices/How_to_Update_Orin_Nano_Developer_Kit_to_Super_Kit',
                 'Edge/NVIDIA_Jetson/Other_Devices/RTL8822CE_Wireless_Module_for_Jetson',
+                'Edge/NVIDIA_Jetson/Other_Devices/RTL8852BE_Wireless_Module_for_Jetson',
                 'Edge/NVIDIA_Jetson/Other_Devices/RM520N_Module_for_Jetson',
                 'Edge/NVIDIA_Jetson/Other_Devices/Jetson_AGX_Orin_32GB_H01_Flash_Jetpack',
                 'Edge/NVIDIA_Jetson/Other_Devices/Jetson_Xavier_AGX_H01_Driver_Installation',
