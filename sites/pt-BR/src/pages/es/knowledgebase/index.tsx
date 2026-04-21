aa48eb9ab30726f5bf5958584a7e03d41c329473
Citric Li <37475446+limengdu@users.noreply.github.com>
2026-04-21 15:15:23 +0800

    Update: display series wiki frameware (#4503)

--- sites/pt-BR/src/pages/es/knowledgebase/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import NotFound from '@theme/NotFound';
-import KnowledgebasePage from '../../knowledgebase/KnowledgebasePage';
-
-export default function KnowledgebaseEs() {
-  if (process.env.SITE_LANG !== 'es') return <NotFound />;
-  return <KnowledgebasePage locale="es" />;
-}
