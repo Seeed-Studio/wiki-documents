51b471b21396150457525bcf6090a1545ebda0b1
limengdu <747632169@qq.com>
2026-04-21 14:45:22 +0800

    Update: display series wiki frameware

--- sites/es/src/pages/ja/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import NotFound from '@theme/NotFound';
-import Home from '../home/_Home';
-
-export default function JaHomeWrapper() {
-  if (process.env.SITE_LANG !== 'ja') return <NotFound />;
-  return <Home locale="ja" />;
-}
--- sites/pt-BR/src/pages/pt-br/knowledgebase/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import NotFound from '@theme/NotFound';
-import KnowledgebasePage from '../../knowledgebase/KnowledgebasePage';
-
-export default function KnowledgebasePtBr() {
-  if (process.env.SITE_LANG !== 'pt-br') return <NotFound />;
-  return <KnowledgebasePage locale="pt-br" />;
-}
