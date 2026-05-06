604a6001a59aa48afecb116f27c9a469e6ceb428
Frank Nie <a487911890@gmail.com>
2026-05-06 11:20:37 +0800

    update sidebars

--- sites/pt-BR/sidebars.js
+++ sites/pt-BR/sidebars.js
@@ -4293,6 +4293,7 @@ const sidebars = {
                 'Robotics/Robot_Kits/reBot_Arm/B601_DM/pt_reBot_Arm_B601_DM_pinocchio',
                 'Robotics/Robot_Kits/reBot_Arm/B601_DM/pt_reBot_Arm_B601_DM_Lerobot',
                 'Robotics/Robot_Kits/reBot_Arm/B601_DM/pt_reBot_Arm_B601_DM_Grasping_Demo',
+                'Robotics/Robot_Kits/reBot_Arm/B601_DM/pt_reBot_Arm_B601_DM_ROS2_Integration',
               ]
             }
           ]
