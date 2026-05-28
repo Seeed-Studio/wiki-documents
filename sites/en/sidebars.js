0dbfcf2259163af994f8e60789ab444c922b5aa3
KasunThushara <kasunthushara16855@gmail.com>
2026-05-28 16:39:07 +0800

    add : flex xio http stream

--- sites/en/sidebars.js
+++ sites/en/sidebars.js
@@ -2094,6 +2094,7 @@ const sidebars = {
                 'Sensor/reSpeaker_flex/reSpeaker_flex_i2s_test',
                 'Sensor/reSpeaker_flex/reSpeaker_flex_xiao_playback_record',
                 'Sensor/reSpeaker_flex/reSpeaker_flex_xiao_udp_stream',
+                'Sensor/reSpeaker_flex/reSpeaker_flex_xiao_http_stream',
               ],
             },
             'Sensor/reSpeaker_flex/reSpeaker_flex_python_control',
