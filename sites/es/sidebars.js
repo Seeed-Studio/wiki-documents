0268154bf68d54046de5f437698610e58bf4ba18
Frank Nie <1447996181@qq.com>
2026-06-18 10:06:34 +0800

    update sidebars

--- sites/es/sidebars.js
+++ sites/es/sidebars.js
@@ -3984,6 +3984,7 @@ const sidebars = {
                   items: [
                     'Edge/NVIDIA_Jetson/reServer_Jetson_Series/reServer_Industrial_J30_J40/es_reServer_Industrial_Getting_Started',
                     'Edge/NVIDIA_Jetson/reServer_Jetson_Series/reServer_Industrial_J30_J40/es_reServer_Industrial_Hardware_Interface_Usage',
+                    'Edge/NVIDIA_Jetson/reServer_Jetson_Series/reServer_Industrial_J30_J40/es_reServer_Industrial_POE_Camera_Usage',
                   ],
                 },
                 {
