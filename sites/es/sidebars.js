df2b440d939084d21950388fad61dc7935395d1e
Frank Nie <1447996181@qq.com>
2026-07-24 09:14:15 +0800

    fix error

--- sites/es/sidebars.js
+++ sites/es/sidebars.js
@@ -4226,7 +4226,7 @@ const sidebars = {
             'Edge/NVIDIA_Jetson/FAQs/es_Overview_of_the_Relationship_Between_JetPack_and_Jetson',
             'Edge/NVIDIA_Jetson/FAQs/es_How_to_Establish_the_Ethercat_on_Jetson',
             'Edge/NVIDIA_Jetson/FAQs/es_Make_DIY_BSP_for_Jetson',
-            'Edge/NVIDIA_Jetson/FAQs/es_Make_DIY_BSP_from_Orin_Nano_DevKit_to_reComputer_Classic',
+            'Edge/NVIDIA_Jetson/FAQs/es_Make_DIY_BSP_from_Orin_Nano_DevKit_to_reComputer_Classic_And_Super',
             'Edge/NVIDIA_Jetson/FAQs/es_How_to_Build_and_Flash_BSP_Source_for_Jetson_Thor',
             'Edge/NVIDIA_Jetson/FAQs/es_Flash_JetPack_with_WSL2',
           ]
