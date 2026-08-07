5e5f7730b51a746f1538c86138b24a2c95b3ff2f
vdmaas98 <159767407+vdmaas98@users.noreply.github.com>
2026-08-07 14:42:10 +0200

    docs: add FAQ for using Arducam OV9281 (Jetvariety) with the A603 carrier board

--- sites/en/sidebars.js
+++ sites/en/sidebars.js
@@ -4330,6 +4330,7 @@ const sidebars = {
             'Edge/NVIDIA_Jetson/FAQs/Solution_for_the_Compatibility_Issue_between_reComputer_and_VEYE_Camera',
             'Edge/NVIDIA_Jetson/FAQs/How_to_use_Camera_IMX219',
             'Edge/NVIDIA_Jetson/FAQs/Use_IMX477_Camera_with_A603',
+            'Edge/NVIDIA_Jetson/FAQs/Use_Arducam_OV9281_Camera_with_A603',
             'Edge/NVIDIA_Jetson/FAQs/Differences_of_L4T_Between_Seeed_and_NVIDIA',
             'Edge/NVIDIA_Jetson/FAQs/Overview_of_the_Relationship_Between_JetPack_and_Jetson',
             'Edge/NVIDIA_Jetson/FAQs/How_to_Establish_the_Ethercat_on_Jetson',
