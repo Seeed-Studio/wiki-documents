4309341556e5f472ff2a9f2626d4d9b29ab9151d
Frank Nie <1447996181@qq.com>
2026-08-11 14:44:11 +0800

    update sidebars

--- sites/es/sidebars.js
+++ sites/es/sidebars.js
@@ -4314,6 +4314,7 @@ const sidebars = {
             'Edge/NVIDIA_Jetson/FAQs/es_Create_Backup_and_Restore_on_reComputer',
             'Edge/NVIDIA_Jetson/FAQs/es_Migrate_Home_Data_from_Jetson_Orin_Nano_Developer_Kit_to_reComputer',
             'Edge/NVIDIA_Jetson/FAQs/es_Deploy_OTA_ON_reComputer',
+            'Edge/NVIDIA_Jetson/FAQs/es_Headless_Setup_and_Recovery_for_A603',
             // 'Edge/NVIDIA_Jetson/FAQs/Updating_Jetpack_with_OTA',
             'Edge/NVIDIA_Jetson/FAQs/es_Solution_for_the_Compatibility_Issue_between_reComputer_and_VEYE_Camera',
             'Edge/NVIDIA_Jetson/FAQs/es_How_to_use_Camera_IMX219',
--- sites/ja/sidebars.js
+++ sites/ja/sidebars.js
@@ -4315,6 +4315,7 @@ const sidebars = {
             'Edge/NVIDIA_Jetson/FAQs/ja_Create_Backup_and_Restore_on_reComputer',
             'Edge/NVIDIA_Jetson/FAQs/ja_Migrate_Home_Data_from_Jetson_Orin_Nano_Developer_Kit_to_reComputer',
             'Edge/NVIDIA_Jetson/FAQs/ja_Deploy_OTA_ON_reComputer',
+            'Edge/NVIDIA_Jetson/FAQs/ja_Headless_Setup_and_Recovery_for_A603',
             // 'Edge/NVIDIA_Jetson/FAQs/Updating_Jetpack_with_OTA',
             'Edge/NVIDIA_Jetson/FAQs/ja_Solution_for_the_Compatibility_Issue_between_reComputer_and_VEYE_Camera',
             'Edge/NVIDIA_Jetson/FAQs/ja_How_to_use_Camera_IMX219',
