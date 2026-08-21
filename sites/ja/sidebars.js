0a98c983eb625dc382678fba1fb47c0379629219
Frank Nie <1447996181@qq.com>
2026-08-21 17:16:34 +0800

    update sidebars

--- sites/ja/sidebars.js
+++ sites/ja/sidebars.js
@@ -1318,7 +1318,7 @@ const sidebars = {
               label: 'ワイヤレス接続',
               items: [
                 'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32C5/Wireless_Connection/ja_XIAO_ESP32C5_WiFi_Usage',
-                'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32C5/Wireless_Connection/ja_XIAO_ESP32C5_Buletooth_Usage',
+                'Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32C5/Wireless_Connection/ja_XIAO_ESP32C5_Bluetooth_Usage',
               ],
             },
             {
