edf5fe1b3370e53a6517de0618b54e8d95e5fe33
ray <lei.he@seeed.cc>
2026-08-27 12:11:38 +0800

    docs: add reSpeaker Clip RTC streaming guide

--- sites/en/sidebars.js
+++ sites/en/sidebars.js
@@ -2180,6 +2180,7 @@ const sidebars = {
           items: [
             'Sensor/reSpeaker_clip/respeaker_clip_getting_started',
             'Sensor/reSpeaker_clip/respeaker_clip_basic_sdk_guide',
+            'Sensor/reSpeaker_clip/respeaker_clip_rtc_streaming',
             'Sensor/reSpeaker_clip/respeaker_clip_service_integration',
             'Sensor/reSpeaker_clip/respeaker_clip_sensecraft_voice_sdk',
             {
